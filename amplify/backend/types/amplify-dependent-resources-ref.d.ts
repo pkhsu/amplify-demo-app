5f1064eb07b03ee8caa911bf1f502efbeaeceebb
kevin.pk.hsu <kevin.pk.hsu@deltaww.com>
2022-08-30 17:04:59 +0800

    add amplify auth and apis

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,38 @@
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "amplifyDemo": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "amplifydemoapp49de8db0": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "amplifyDemoApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    },
+    "auth": {
+        "amplifydemoapp78316cef": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
